511c2e9fdace99bf168b1d9b16bc5e1e7a93c1e6
Tom Gustafsson <tom@valdemar.hut.fi>
2016-03-07 18:19:40 +0200

    Started fixing tests and preparing first simple Poisson example.

--- fem/__init__.py
+++ fem/__init__.py
@@ -1,29 +1,10 @@
-## \mainpage Introduction
-#
-# Starting the refactoring, cleaning and adding of tests/examples.
-#
-# The major plan: test_module.py for full problem setup tests that
-# simultaneously are used as documentation/examples.
-#
-# test_asm.py, test_mesh.py etc. for smaller and more concentrated unit tests.
-#
-# to-remove-list: AssemblerTri, GeometryComsol
-#
-# to-refactor-list: Elements (increase consistency in naming).
-# GeometryPSLG2D->GeometryTriangle2D
-#
-# Steps:
-# 1. Remove and change names.
-# 2. Fix tests
-# 3. Add few good examples/module level tests
-
 ## \page gettingstarted Getting started
 #
 # Add some simple example here.
 
 ## \page tips Tips
 #
-# \section Problems with Mayavi or other graphical toolbox
+# \section tip1 Problems with Mayavi or other graphical toolbox
 #
 # Errors related to qt4, wx, mayavi, etc. can be sometimes
 # fixed by simply changing environment variables or running
@@ -33,7 +14,7 @@
 #
 # ETS_TOOLKIT=qt4 ipython --gui=wx --pylab=wx
 #
-# \section Running the tests
+# \section tip2 Running the tests
 #
 # Simplest way is to discover all the tests using unittest as follows:
 #
---
+++ fem/example_asm.py
@@ -0,0 +1,39 @@
+## \example example_asm_poisson.py
+#
+# In the following example we solve the Poisson equation in a three-dimensional
+# box domain \f$\Omega=[0,1]^3\f$. We construct a simple manufactured solution
+# and then compute the convergence rates
+# of the finite element solution in the \f$H^1\f$-norm.
+#
+# We consider the following partial differential equation: find
+# \f$u:\Omega\rightarrow\mathbb{R}\f$ that satisfies
+# \f[
+#     -\Delta u = f \quad \text{in $\Omega$,} \quad \text{and} \quad \frac{\partial u}{\partial n}=g-u \quad \text{on $\partial\Omega$.}
+# \f]
+# The weak formulation of the problem is: find \f$u\in H^1(\Omega)\f$ that satisfies
+# \f[
+#     (\nabla u, \nabla v) + \langle u, v \rangle = (f,v) + \langle g, v \rangle,
+# \f]
+# for every \f$v \in H^1(\Omega)\f$.
+#
+# Through symbolic computation of the Laplacian one can verify that
+# the solution \f$u(x,y,z)=1+x-x^2y^2+x y z^3\f$
+# is given by the following data:
+# \f[
+#     f(x,y,z)=2x^2+2y^2-6xyz, \quad \text{and}
+#     \quad g(x,y,z)=\begin{cases}
+#           3-3y^2+2yz^3 & \text{if $x=1$,} \\
+#           -yz^3 & \text{if $x=0$,} \\
+#           1+x-3x^2+2xz^3 & \text{if $y=1$,} \\
+#           1+x-xz^3 & \text{if $y=0$,} \\
+#           1+x+4xy-x^2y^2 & \text{if $z=1$,} \\
+#           1+x-x^2y^2 & \text{if $z=0$.}
+#     \end{cases}
+# \f]
+# In the following example, we compute the finite element solution \f$u_h\f$ using
+# tetrahedral \f$P_k\f$ elements, \f$k \in \{1,2\}\f$. We evaluate the error
+# \f$\|u-u_h\|_{H^1(\Omega)}\f$
+# and verify the a priori error estimate
+# \f[
+#   \|u-u_h\|_{H^1(\Omega)} \leq C h^k |u|_2.
+# \f]
