c693703c48afff8c761528c2fefd7860299108db
Tom Gustafsson <tom@valdemar.hut.fi>
2017-12-13 09:38:19 +0200

    Added some classes to __all__ for automatic importing

--- spfem/__init__.py
+++ spfem/__init__.py
@@ -1,1 +1,6 @@
-__all__ = ['mesh', 'assembly', 'element', 'utils']
+from mesh import *
+from assembly import *
+from element import *
+from utils import *
+
+__all__ = ['MeshTri', 'MeshTet', 'MeshQuad', 'MeshLine', 'AssemblerElement', 'AssemblerAbstract', 'ElementTriP1', 'ElementTetP1', 'AbstractElementTriPp', 'AbstractElementTriDG', 'ElementTriDG', 'ElementTetDG', 'direct']
